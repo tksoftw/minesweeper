7aafc83ba1131daefb802afdbb514cb562d8102c
Library <library@library04.local>
2023-03-08 13:28:34 -0500

    update

--- minesweeper.py
+++ minesweeper.py
@@ -6,20 +6,27 @@ class Position:
 	
 	def __init__(self, pos_str):
 		self.x = self.p_table[pos_str[0].upper()]
-		self.y = int(pos_str[1])
+		self.y = int(pos_str[1])-1 # accounting for 0-indexing
 
 class Grid:
-	def __init__(self):
-		self.grid = [['-']*8 for _ in range(8)]
-		self.viewable_grid = [["-"]*8 for _ in range(8)]
+	def __init__(self, dim=8):
+		self.grid = [['-']*dim for _ in range(dim)]
+		self.viewable_grid = [["-"]*dim for _ in range(dim)]
+		self.hidden_tiles = {(i,j) for j in range(dim) for i in range(dim)}
+		self.mine_count = 0
 		self.valleys = []
 
+		# prep functions
+		self._scatter_mines()
+		self._calc_numbers()
+		self.valleys = self._get_valleys()
 
 	def _scatter_mines(self):
 		for _ in range(10):
 			i = random.randint(0, len(self.grid)-1)
 			j = random.randint(0, len(self.grid[i])-1)
 			self.grid[i][j] = '*'
+			self.mine_count += 1
 
 	def _get_adj_inds(self, i, j, edges=True):
 		mat = self.grid
@@ -82,65 +89,84 @@ class Grid:
 				return valley
 		raise ValueError(f"No valley found containing tile: ({pos.x}, {pos.y})")
 
-	def _get_lake_by_tile(self, pos: Position):
-		for lake in lakes:
-			if (pos.x, pos.y) in lake:
-				return lake
-		raise ValueError(f"No lake found containing tile: ({pos.x}, {pos.y})")
-
 	def _print(self):
 		for row in self.grid:
 			print(' '.join(row))
 
+	def _is_mine(self, pos: Position):
+		return self.grid[pos.x][pos.y] == '*'
+
 	def print(self):
-		print(' '*2 + ' '.join(str(n) for n in range(8)))
+		print(' '*2 + ' '.join(str(n) for n in range(1, 9)))
 		for i, row in enumerate(self.viewable_grid):
 			print(string.ascii_uppercase[i], ' '.join(row))
 
-	def flag(self, pos_str):
-		pos = Position(pos_str)
+	def flag(self, pos: Position):
 		if self.viewable_grid[pos.x][pos.y] == '-':
 			self.viewable_grid[pos.x][pos.y] = '#'
 	
-	def unflag(self, pos_str):
-		pos = Position(pos_str)
+	def unflag(self, pos: Position):
 		if self.viewable_grid[pos.x][pos.y] == '#':
 			self.viewable_grid[pos.x][pos.y] = '-'
 
-	def remove_tile(self, pos_str):
-		pos = Position(pos_str)
+	def remove_tile(self, pos: Position):
 		real_tile = self.grid[pos.x][pos.y]
 		to_apply = [(pos.x, pos.y)]
 		if real_tile == '.':
 			print("you found a lake!")
-			lake = self._get_lake_by_tile(pos)
+			lake = self._get_valley_by_tile(pos)
 			to_apply = lake
 		for x, y in to_apply:
 			self.viewable_grid[x][y] = self.grid[x][y]
-		
+			self.hidden_tiles.discard((x,y))
 	
+	def is_completed_board(self):
+		return len(self.hidden_tiles) == self.mine_count
+
+	def play_game(self):
+		print('Welcome to Minesweeper')
+		while not self.is_completed_board():
+			print('Current board:')
+			self.print()
+			c = input('\n'.join(["What would you like to do?", "->Explore a tile (E)", "->Place a flag (F)", "->Remove a flag (R)\n"]))
+			if c.upper() not in ('E', 'F', 'R'):
+				continue
+			p = input('Enter a position (e.g. A3): ')
+			pos = Position(p)
+			
+			if c.upper() == 'E':
+				self.remove_tile(pos)
+				if self._is_mine(pos):
+					print("You hit a mine! You lose")
+					return
+			elif c.upper() == 'F':
+				self.flag(pos)
+			else:
+				self.unflag(pos)
 
 
 
 
 if __name__ == '__main__':
 	g = Grid()
-	g.print()
-	g.flag('A5')
-	g.print()
-	print('mmmm')
-	g._scatter_mines()
-	g._print()
-	print('nnnn')
-	g._calc_numbers()
-	g._print()
-	print('oooo')
-	lakes = g._get_valleys()
-	g._print()
-	for i, lake in enumerate(lakes):
-		print(f"Lake {string.ascii_uppercase[i]}:", lake)
-	a = 0
-	while a < 10:
-		g.print()
-		p = input('Enter a position: ')
-		g.remove_tile(p)
+	#g.print()
+	#g.flag(Position('A5'))
+	#g.print()
+	#print('mmmm')
+	#g._scatter_mines()
+	#g._print()
+	#print('nnnn')
+	#g._calc_numbers()
+	#g._print()
+	#print('oooo')
+	#lakes = g._get_valleys()
+	#g._print()
+	#for i, lake in enumerate(lakes):
+	#	print(f"Lake {string.ascii_uppercase[i]}:", lake)
+	#a = 0
+	#while a < 2:
+	#	g.print()
+	#	p = input('Enter a position: ')
+	#	g.remove_tile(Position(p))
+	#	a += 1
+	g.play_game()
