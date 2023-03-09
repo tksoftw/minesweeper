5517bcaf0c68df0714604b35fde4567cfd0e1ab2
Library <library@library04.local>
2023-03-09 12:55:00 -0500

    update

--- ms-gui.py
+++ ms-gui.py
@@ -1,33 +1,40 @@
+from minesweeper import Grid
 import pygame
 pygame.init()
 
-width, height = 500, 500
-screen = pygame.display.set_mode((width, height))
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+def draw_everything():
+	width, height = 512, 512
+	screen = pygame.display.set_mode((width, height))
 
-ball = pygame.image.load("ball.png")
-rect = ball.get_rect()
-speed = [1, 1]
-rect = rect.move(speed)
+def start_game():
+	g = Grid()
 
+
+def draw_grid(g: Grid):
+	box_size = 512//8
+	screen = pygame.display.get_surface()
+
+	mat = [[0]*8 for _ in range(8)]
+	for x, row in enumerate(mat):
+		for y, v in enumerate(row):
+			box = pygame.Rect(x*box_size, y*box_size, box_size, box_size)
+			pygame.draw.rect(screen, (0, 255, 255), box, 2)
+	
+	pygame.display.update()
+
+draw_everything()
+draw_grid(Grid())
+
+screen = pygame.display.get_surface()
 running = True
 while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
-	rect = rect.move(speed)
-	if rect.left < 0 or rect.right > width:
-		speed[0] = -speed[0]
-	if rect.top < 0 or rect.bottom > height:
-		speed[1] = -speed[1]
-
-	screen.fill(GREEN)
-	pygame.draw.rect(screen, RED, rect, 1)
-	screen.blit(ball, rect)
-	pygame.display.update()
+	
+				
+	
 
 
 
@@ -37,9 +44,11 @@ pygame.quit()
 
 """
 if event.type == pygame.MOUSEMOTION:
+			print(event.pos)
 			r, g = [p // 2 for p in event.pos]
 			b = (r + g) % 256
 			color = (r, g, b)
 			screen.fill(color)
 			pygame.display.update()
+
 """
