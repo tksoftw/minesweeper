a5ef479f1d5be3bba15b06d73093a04c84f5aaed
Library <library@library04.local>
2023-03-09 14:55:18 -0500

    update

--- ms-gui.py
+++ ms-gui.py
@@ -2,24 +2,37 @@ from minesweeper import Grid
 import pygame
 pygame.init()
 
+COLORS = {
+	'black': (0,0,0),
+	'white': (255,255,255),
+	'grey': (192,192,192)
+}
+
 
 def draw_everything():
 	width, height = 512, 512
 	screen = pygame.display.set_mode((width, height))
+	screen.fill(COLORS['grey'])
 
 def start_game():
 	g = Grid()
 
+def draw_grid(g: Grid):	
+	screen_length = 512
+	outer_border=5+screen_length%8*2
+	available_length = screen_length-2*outer_border
+	print(screen_length, available_length)
+	box_size = available_length//8
 
-def draw_grid(g: Grid):
-	box_size = 512//8
 	screen = pygame.display.get_surface()
 
 	mat = [[0]*8 for _ in range(8)]
 	for x, row in enumerate(mat):
 		for y, v in enumerate(row):
-			box = pygame.Rect(x*box_size, y*box_size, box_size, box_size)
-			pygame.draw.rect(screen, (0, 255, 255), box, 2)
+			xpos = x*box_size + outer_border
+			ypos = y*box_size + outer_border
+			box = pygame.Rect(xpos, ypos, box_size, box_size)
+			pygame.draw.rect(screen, COLORS['black'], box, 1)
 	
 	pygame.display.update()
 
