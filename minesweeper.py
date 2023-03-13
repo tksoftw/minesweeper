7a94af46afcc97df0f03c26108f4bd53cf93051e
tksoftw <tkennedy4432@gmail.com>
2023-03-12 22:29:29 -0400

    update

--- minesweeper.py
+++ minesweeper.py
@@ -1,6 +1,13 @@
 import random
 import string
 
+class ValleyManager:
+	def __init__(self):
+		self.valleys = []
+		self.enterances = {}
+	
+		
+
 class Position:
 	p_table = {c: i for i, c in enumerate(string.ascii_uppercase)}
 	
@@ -10,22 +17,24 @@ class Position:
 
 	@classmethod
 	def from_str(cls, pos_str):
-		px = self.p_table[pos_str[0].upper()]
+		px = cls.p_table[pos_str[0].upper()]
 		py = int(pos_str[1])-1 # accounting for 0-indexing
 		return Position(py, px)
 
 class Grid:
-	def __init__(self, dim=8, mines=10):
+	def __init__(self, dim=8, mines=10, debug_mode=False):
 		self.grid = [['-']*dim for _ in range(dim)]
 		self.viewable_grid = [["-"]*dim for _ in range(dim)]
 		self.hidden_tiles = {(i,j) for j in range(dim) for i in range(dim)}
 		self.mine_count = 0
 		self.valleys = []
+		self.debug_mode = debug_mode
 
 		# prep functions
-		self._scatter_mines(mines)
-		self._calc_numbers()
-		self.valleys = self._get_valleys()
+		if not self.debug_mode:
+			self._scatter_mines(mines)
+			self._calc_numbers()
+			self.valleys = self._get_valleys()
 
 	def _scatter_mines(self, mines_to_place):
 		placed = set()
@@ -83,7 +92,7 @@ class Grid:
 			curr_set.update(self._get_valley_set(adj_i, adj_j, curr_set, replace_ch))
 		return curr_set
 
-	def _get_valleys(self):
+	def _get_valleys_old(self):
 		nums = range(26)
 		valleys = []
 		for row_n, row in enumerate(self.grid):
@@ -94,11 +103,80 @@ class Grid:
 
 		return valleys
 
+	def _add_valley_tile(self, i, j, valleys, enterances):
+		tile = (i, j)
+		#print('CT', tile)
+		valley = None
+		if tile in enterances:
+			valley = enterances.get(tile)
+			valley.append(tile)
+		else:
+			#print('happening!!!!!!!!!!!!!!')
+			valleys.append([(tile)])
+			valley = valleys[-1]
+
+		#print('ADJs')
+		for adj in self._get_adj_inds(*tile):	
+			#print(adj)
+			#print(valleys)
+			if adj in enterances and enterances[adj] != valley:
+				#print('FIXING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+				#print('adj', adj, ':', enterances[adj])
+				#print('val', valley)
+				precursor_valley = enterances[adj]
+				valley_n = valleys.index(precursor_valley)
+				#print(valley)
+				#print(precursor_valley)
+				valley = precursor_valley
+				precursor_valley.extend(valleys[valley_n])
+				#print('oneeee', valley)
+				#print('twoooo', precursor_valley)
+			elif self.grid[adj[0]][adj[1]] == '-':
+				enterances[adj] = valley
+
+	def _add_valley_iter_dfs(self, i, j, valleys):
+		new_valley = set()
+		bounds = len(self.grid)
+		visited = [False]*bounds**2
+		stack = [i*bounds+j]
+
+		self.grid[i][j] = '.'
+
+		while len(stack):
+			v = stack.pop()
+			i, j = divmod(v, bounds)
+			print(i, j)
+			if not visited[v]:
+				new_valley.add((i,j))
+				visited[v] = True
+
+			last = self.grid[i][j]
+			for p, q in self._get_adj_inds(i, j):
+				if visited[p*bounds+q]:
+					continue
+				if self.grid[p][q] == '-':
+					self.grid[p][q] = '.'
+					stack.append(p*bounds+q)
+				elif self.grid[p][q] != '*' and last == '.': 
+					stack.append(p*bounds+q)
+		valleys.append(new_valley)
+
+
+	def _get_valleys(self):
+		valleys = []
+		for i, row in enumerate(self.grid):
+			for j, v in enumerate(row):
+				if v == '-':
+
+					self._add_valley_iter_dfs(i,j,valleys)
+		print(valleys)
+		return valleys
+
 	def _get_valley_by_tile(self, pos: Position):
 		for valley in self.valleys:
 			if (pos.x, pos.y) in valley:
 				return valley
-		raise ValueError(f"No valley found containing tile: ({pos.x}, {pos.y})")
+		return -1
 
 	def _print(self):
 		for row in self.grid:
@@ -112,9 +190,12 @@ class Grid:
 		for i, row in enumerate(self.viewable_grid):
 			print(string.ascii_uppercase[i], ' '.join(row))
 
-	def flag(self, pos: Position):
+	def flag(self, pos: Position, unflag_if_flagged=False):
+		print(self.viewable_grid)
 		if self.viewable_grid[pos.x][pos.y] == '-':
 			self.viewable_grid[pos.x][pos.y] = '#'
+		elif self.viewable_grid[pos.x][pos.y] == '#' and unflag_if_flagged:
+			self.unflag(pos)
 	
 	def unflag(self, pos: Position):
 		if self.viewable_grid[pos.x][pos.y] == '#':
@@ -124,12 +205,12 @@ class Grid:
 		real_tile = self.grid[pos.x][pos.y]
 		to_apply = [(pos.x, pos.y)]
 		if real_tile == '.':
-			print("you found a lake!")
 			lake = self._get_valley_by_tile(pos)
 			to_apply = lake
 		for x, y in to_apply:
 			self.viewable_grid[x][y] = self.grid[x][y]
 			self.hidden_tiles.discard((x,y))
+		return real_tile != '*'
 	
 	def is_completed_board(self):
 		return len(self.hidden_tiles) == self.mine_count
@@ -159,25 +240,36 @@ class Grid:
 
 
 if __name__ == '__main__':
-	g = Grid()
-	#g.print()
-	#g.flag(Position('A5'))
-	#g.print()
-	#print('mmmm')
-	#g._scatter_mines()
-	#g._print()
-	#print('nnnn')
-	#g._calc_numbers()
-	#g._print()
-	#print('oooo')
-	#lakes = g._get_valleys()
-	#g._print()
-	#for i, lake in enumerate(lakes):
-	#	print(f"Lake {string.ascii_uppercase[i]}:", lake)
-	#a = 0
-	#while a < 2:
-	#	g.print()
-	#	p = input('Enter a position: ')
-	#	g.remove_tile(Position(p))
-	#	a += 1
-	g.play_game()
+	g = Grid(debug_mode=True)
+	if g.debug_mode:
+		g.print()
+		g.flag(Position.from_str('A5'))
+		g.print()
+		print('mmmm')
+		#g._scatter_mines(10)
+		#g._print()
+		print('nnnn')
+		#g._calc_numbers()
+		g._print()
+		g.grid = [['-','-','-','1','*','2','1','-',],
+				['-','-','-','1','2','*','1','-',],
+				['-','1','1','1','1','1','-','-',],
+				['1','3','*','2','-','-','-','-',],
+				['1','*','*','3','1','1','2','2',],
+				['1','2','3','*','2','2','*','-',],
+				['-','-','2','3','*','2','-','-',],
+				['-','-','1','*','-','-','-','-',]]	
+		print('oooo')
+		g._print()
+		lakes = g._get_valleys()
+		g._print()
+		for i, lake in enumerate(lakes):
+			print(f"Lake {string.ascii_uppercase[i]}:", lake)
+		a = 0
+		while a < 2:
+			g.print()
+			p_str = input('Enter a position: ')
+			g.remove_tile(Position.from_str(p_str))
+			a += 1
+	else:
+		g.play_game()
--- ms-gui.py
+++ ms-gui.py
@@ -8,18 +8,12 @@ COLORS = {
 	'grey': (192,192,192),
 	'red': (255,0,0),
 	'green': (0,255,0),
-	'blue': (0,0,255)
+	'blue': (0,0,255),
+	'light_grey': (138,138,138)
 }
-
-
-def draw_everything():
-	width, height = 500, 500
-	screen = pygame.display.set_mode((width, height))
-	screen.fill(COLORS['grey'])
 
 def start_game():
 	g = Grid()
-
 
 def get_border_padding(screen_length, const_border, boxes_per_row):
 	W = screen_length
@@ -31,6 +25,10 @@ def get_border_padding(screen_length, const_border, boxes_per_row):
 	return x
 
 def draw_grid(g: Grid):	
+	width, height = 500, 500
+	screen = pygame.display.set_mode((width, height))
+	screen.fill(COLORS['light_grey'])
+
 	L = 500
 	C = 30
 	A = len(g.grid)
@@ -41,18 +39,31 @@ def draw_grid(g: Grid):
 	print(box_size)
 
 	screen = pygame.display.get_surface()
+	font = pygame.font.Font(pygame.font.get_default_font(), 36)
 
 	for i, row in enumerate(g.grid):
 		for j, v in enumerate(row):
+			viewable = g.viewable_grid[i][j]
 			ypos = i*box_size + outer_border
 			xpos = j*box_size + outer_border
-			box = pygame.Rect(ypos, xpos, box_size, box_size)
-			if v == '*' and g.viewable_grid[i][j] == '#':
-				pygame.draw.rect(screen, COLORS['blue'], box)
-			elif v == '*':
-				pygame.draw.rect(screen, COLORS['red'], box)	
-			elif g.viewable_grid[i][j] == '#':
-				pygame.draw.rect(screen, COLORS['green'], box)
+			box = pygame.Rect(xpos, ypos, box_size, box_size)
+			if viewable == '*':
+				pygame.draw.rect(screen, COLORS['red'], box)
+				mine = font.render('*', True, COLORS['white'])
+				aligner = mine.get_rect(center=(box.centerx, box.centery))
+				screen.blit(mine, aligner)
+			elif viewable == '#':
+				flag = font.render('#', True, COLORS['red'])
+				aligner = flag.get_rect(center=(box.centerx, box.centery))
+				screen.blit(flag, aligner)
+			elif viewable == '.':
+				pygame.draw.rect(screen, COLORS['grey'], box)
+			elif viewable.isdigit():
+				pygame.draw.rect(screen, COLORS['grey'], box)
+				warning_number = font.render(v, True, COLORS['black'])
+				aligner = warning_number.get_rect(center=(box.centerx, box.centery))
+				screen.blit(warning_number, aligner)
+				
 			pygame.draw.rect(screen, COLORS['black'], box, 1)
 	
 	pygame.display.update()
@@ -66,13 +77,12 @@ def get_box_inds_from_pos(x, y, window_width, border_length, boxes_per_row):
 	avail = int(L-2*B) # should always be an whole number anyway
 	box_size = avail//A 
 	print(box_size)
-	row = ((y+B)//box_size)-1 # -1 to account for 0-indexing
-	col = ((x+B)//box_size)-1
+	row = ((x+B)//box_size)-1 # -1 to account for 0-indexing
+	col = ((y+B)//box_size)-1
 	return (row, col)
 
 
 g = Grid(8, 10)
-draw_everything()
 draw_grid(g)
 
 screen = pygame.display.get_surface()
@@ -82,13 +92,17 @@ while running:
 		if event.type == pygame.QUIT:
 			running = False
 	
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			print(f'you clicked @: {event.pos}!')
+		if event.type == pygame.MOUSEBUTTONDOWN and event.button in (1,3):
 			box_inds = get_box_inds_from_pos(*event.pos, 500, 30, 8)
-			print('box inds:', box_inds)
-			g.flag(Position(*box_inds))
+			p = Position(*box_inds)
+			if event.button == 1:
+				not_removed_mine = g.remove_tile(p)	
+				running = not_removed_mine
+			elif event.button == 3:
+				g.flag(p, unflag_if_flagged=True)
 			draw_grid(g)
 
+input()
 
 pygame.quit()
 
